5d18dcb6b1c3f3ca14f28afd4c0b6dec556580e5
martynukvaleria <martynukvalery2004@gmail.com>
2024-03-01 15:45:50 +0300

    search page not working

--- test-1/src/test/java/web/elements/TextField.java
+++ test-1/src/test/java/web/elements/TextField.java
@@ -1,7 +1,20 @@
 package web.elements;
 
+import core.DriverUtils;
+
 public class TextField extends PageElement {
-    public TextField(String locator) {
-        super(locator);
+    public TextField(String name, String locator) {
+        super(name, locator);
+    }
+    public void sendKeys(String text){
+        DriverUtils.sendKeys(text, locator);
+    }
+
+    public void clickEnter(String locator) {
+        DriverUtils.clickEnter(locator);
+    }
+
+    public String getLocator() {
+        return locator;
     }
 }
